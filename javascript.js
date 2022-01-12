a719dfeb1de8241b53b657d2525fac9a70626dee
Rafael <rafael.berger20@gmail.com>
2022-01-12 16:00:10 -0300

    feita a seleção dos dados das respostas

--- javascript.js
+++ javascript.js
@@ -1,38 +1,38 @@
-const primeiraPergunta = {
-  pergunta: "Em qual ano a linguagem JavaScript foi criada?",
-  a: "2015",
-  b: "1970",
-  c: "1995",
-  d: "1983",
-  correta: "c",
-};
-
-const segundaPergunta = {
-  pergunta: "Qual o significado da sigla CSS?",
-  a: "Cascading Style Sheets",
-  b: "Hypertext Markup Language",
-  c: "JavaScript Object Notation",
-  d: "Cab Small Snacks",
-  correta: "a",
-};
-
-const terceiraPergunta = {
-  pergunta: "Qual das opções é uma linguagem de programação?",
-  a: "CSS",
-  b: "Python",
-  c: "HTML",
-  d: "API",
-  correta: "b",
-};
-
-const quartaPergunta = {
-  pergunta: "O Desenvolvedor Web que trabalha com o Client Side é considerado:",
-  a: "Front-end",
-  b: "Fullstack",
-  c: "Back-end",
-  d: "Nenhuma das anteriores",
-  correta: "a",
-};
+const dadosQuiz = [
+  {
+    pergunta: "Em qual ano a linguagem JavaScript foi criada?",
+    a: "2015",
+    b: "1970",
+    c: "1995",
+    d: "1983",
+    correta: "c",
+  },
+  {
+    pergunta: "Qual o significado da sigla CSS?",
+    a: "Cascading Style Sheets",
+    b: "Hypertext Markup Language",
+    c: "JavaScript Object Notation",
+    d: "Cab Small Snacks",
+    correta: "a",
+  },
+  {
+    pergunta: "Qual das opções é uma linguagem de programação?",
+    a: "CSS",
+    b: "C++",
+    c: "HTML",
+    d: "API",
+    correta: "b",
+  },
+  {
+    pergunta:
+      "O Desenvolvedor Web que trabalha com o Client Side é considerado:",
+    a: "Front-end",
+    b: "Fullstack",
+    c: "Back-end",
+    d: "Nenhuma das anteriores",
+    correta: "a",
+  },
+];
 
 const perguntaEl = document.querySelector("#pergunta");
 const a = document.querySelector("#a");
@@ -42,11 +42,25 @@ const d = document.querySelector("#d");
 const botao = document.querySelector("#btn");
 
 window.addEventListener("load", () => {
-  perguntaEl.innerHTML = primeiraPergunta.pergunta;
-  a.innerHTML = primeiraPergunta.a;
-  b.innerHTML = primeiraPergunta.b;
-  c.innerHTML = primeiraPergunta.c;
-  d.innerHTML = primeiraPergunta.d;
+  //carrega a página com a primeira pergunta
+  perguntaEl.innerText = dadosQuiz[0].pergunta;
+  a.innerText = dadosQuiz[0].a;
+  b.innerText = dadosQuiz[0].b;
+  c.innerText = dadosQuiz[0].c;
+  d.innerText = dadosQuiz[0].d;
 });
 
-botao.addEventListener("click", () => {});
+function selecionar() {
+  const inputsRadio = document.querySelectorAll(".respostas");
+
+  inputsRadio.forEach((resposta) => {
+    //pega o texto do lado do input selecionado
+    if (resposta.checked === true) {
+      console.log(resposta.nextElementSibling.innerText);
+    }
+  });
+}
+
+botao.addEventListener("click", () => {
+  selecionar();
+});
